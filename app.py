ffe0c1c35200fc0b1373f3f13f3e786db9f3cca3
mgudwilowicz <m.gudwilowicz@gmail.com>
2025-12-15 17:34:57 +0100

    add delete method

--- app.py
+++ app.py
@@ -27,6 +27,17 @@ def add():
         return redirect(url_for('index'))
     return render_template('add.html')
 
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    global blog_posts
+    blog_posts = [post for post in blog_posts if post["id"] != post_id]
+
+    with open("data/data.json", "w", encoding="utf-8") as file:
+        json.dump(blog_posts, file, indent=4, ensure_ascii=False)
+
+    return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
