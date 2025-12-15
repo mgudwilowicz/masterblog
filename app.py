00957710cda2ed41d0a9c3f5491234b5ad4ba847
mgudwilowicz <m.gudwilowicz@gmail.com>
2025-12-15 18:30:08 +0100

    add update route & add update.html

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import json
 
 app = Flask(__name__)
 
-
 with open("data/data.json", "r", encoding="utf-8") as file:
     blog_posts = json.load(file)
 
@@ -11,6 +10,7 @@ with open("data/data.json", "r", encoding="utf-8") as file:
 @app.route('/')
 def index():
     return render_template('index.html', posts=blog_posts)
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -27,6 +27,7 @@ def add():
         return redirect(url_for('index'))
     return render_template('add.html')
 
+
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
     global blog_posts
@@ -36,6 +37,38 @@ def delete(post_id):
         json.dump(blog_posts, file, indent=4, ensure_ascii=False)
 
     return redirect(url_for('index'))
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        author = request.form.get('author')
+        content = request.form.get('content')
+
+        for post in blog_posts:
+            if post['id'] == post_id:
+                post['title'] = title
+                post['author'] = author
+                post['content'] = content
+
+        with open("data/data.json", "w", encoding="utf-8") as file:
+            json.dump(blog_posts, file, indent=4, ensure_ascii=False)
+
+        return  redirect('/')
+
+    return render_template('update.html', post=post)
+
+
+def fetch_post_by_id(post_id):
+    post = [post for post in blog_posts if post["id"] == post_id]
+    if post:
+        return post[0]
+    return None
 
 
 
