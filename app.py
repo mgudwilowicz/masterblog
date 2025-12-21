47de84b7fedc9f51725f2478369163e948bc067e
mgudwilowicz <m.gudwilowicz@gmail.com>
2025-12-21 14:02:08 +0100

    add docstring & add delete_post fn

--- app.py
+++ app.py
@@ -9,18 +9,20 @@ with open("data/data.json", "r", encoding="utf-8") as file:
 
 @app.route('/')
 def index():
+    """Show all blog posts on the homepage."""
     return render_template('index.html', posts=blog_posts)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    """Add a new blog post."""
     if request.method == 'POST':
-        id = blog_posts[-1]['id'] + 1
+        new_id = blog_posts[-1]['id'] + 1
         title = request.form.get('title')
         author = request.form.get('author')
         content = request.form.get('content')
 
-        new_post = {"id": id, "title": title, "author": author, "content": content}
+        new_post = {"id": new_id, "title": title, "author": author, "content": content}
         blog_posts.append(new_post)
         with open("data/data.json", "w", encoding="utf-8") as file:
             json.dump(blog_posts, file, indent=4, ensure_ascii=False)
@@ -30,8 +32,8 @@ def add():
 
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
-    global blog_posts
-    blog_posts = [post for post in blog_posts if post["id"] != post_id]
+    """Delete a blog post by ID."""
+    blog_posts = delete_post(post_id)
 
     with open("data/data.json", "w", encoding="utf-8") as file:
         json.dump(blog_posts, file, indent=4, ensure_ascii=False)
@@ -41,6 +43,7 @@ def delete(post_id):
 
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update(post_id):
+    """Update a blog post by ID."""
     post = fetch_post_by_id(post_id)
     if post is None:
         return "Post not found", 404
@@ -59,18 +62,20 @@ def update(post_id):
         with open("data/data.json", "w", encoding="utf-8") as file:
             json.dump(blog_posts, file, indent=4, ensure_ascii=False)
 
-        return  redirect('/')
+        return redirect('/')
 
     return render_template('update.html', post=post)
 
 
 def fetch_post_by_id(post_id):
+    """Return a blog post by ID or None if not found."""
     post = [post for post in blog_posts if post["id"] == post_id]
     if post:
         return post[0]
     return None
 
-
+def delete_post(id):
+    return [post for post in blog_posts if post["id"] != id]
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
